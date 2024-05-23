e4559d3f71af2d7768708425c7218a7afb064f02
Jakub Frelik <git@jfrelik.dev>
2024-05-23 20:07:37 +0200

    feat: add files

---
+++ LinqTutorials/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = LinqTasks.Task6();
+
+            foreach (var VARIABLE in t)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+        }
+    }
+}
